479e5614f322457c0c3d645bbce463a69f206a2c
nagwai832 <30415187+nagwai832@users.noreply.github.com>
2021-04-15 15:04:07 -0600

    login_screen_modified

--- lib/ui/screens/login/login_screen.dart
+++ lib/ui/screens/login/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 //import 'package:mafcode/ui/auto_router_config.gr.dart';
 import 'package:mafcode/ui/shared/logo_widget.dart';
 import 'package:http/http.dart' as http;
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
 
@@ -14,8 +15,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool showSpinner = false;
   String email;
   String password;
+
 
   Dio dio = new Dio();
   Future postData() async{
@@ -39,8 +42,6 @@ class _LoginScreenState extends State<LoginScreen> {
 
 
 
-
-
   }
   @override
   Widget build(BuildContext context) {
@@ -48,42 +49,58 @@ class _LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            LogoWidget(),
-            SizedBox(height: 48),
-            TextField(
-              decoration: InputDecoration(labelText: "Username"),
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: "Password"),
-            ),
-            SizedBox(height: 24),
-            Align(
-              alignment: Alignment.centerRight,
-              child: RaisedButton(
-                color: Theme.of(context).primaryColor,
-                textColor: Colors.white,
-                child: Text("Login"),
-                onPressed: () async{
-                  print('Posting data...');
-                  await postData().then((value) {
-                    print(value);
-                  });
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LogoWidget(),
+              SizedBox(height: 48),
+              TextField(
+                decoration: InputDecoration(labelText: "Username"),
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(labelText: "Password"),
+              ),
+              SizedBox(height: 24),
+              Align(
+                alignment: Alignment.centerRight,
+                child: RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  child: Text("Login"),
+                  onPressed: () async{
+                    setState(() {
+                      showSpinner= true;
+                    });
+                    print('Posting data...');
+                    try {
+                      final logUser= await postData().then((value) {
+                        print(value);
+                      });
+                      if(logUser != null){
+                        Navigator.of(context).pushReplacementNamed(Routes.mainScreen);
+                      }
+                      setState(() {
+                        showSpinner=false;
+                      });
+                    } on Exception catch (e) {
+                      print(e);
+                    }
 //                  await post().then((value){
 //                    print(value);
 //                  });
 
-                  //Navigator.of(context).pushReplacementNamed(Routes.mainScreen);
-                },
-              ),
-            )
-          ],
+
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
